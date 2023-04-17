3667fc9c3443063db92a01cad443a7f8eb5cf98d
Aknur Tazhibek <atazhibek@lenovo.com>
2023-04-18 00:19:58 +0600

    change name

--- src/test/java/common/UICommonTest.java
+++ src/test/java/common/UICommonTest.java
@@ -5,7 +5,7 @@ import org.openqa.selenium.WebDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
-public class UICommon {
+public class UICommonTest {
 	protected WebDriver driver;
 
 	@BeforeMethod()
