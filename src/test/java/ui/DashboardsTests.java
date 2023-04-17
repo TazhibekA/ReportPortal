7cfac0a02eb08dd7d67ee0389039d8b0abefd838
Aknur Tazhibek <atazhibek@lenovo.com>
2023-04-18 00:25:22 +0600

    change name

--- src/test/java/ui/DashboardsTests.java
+++ src/test/java/ui/DashboardsTests.java
@@ -1,10 +1,10 @@
 package ui;
 
 import business.pages.LoginPage;
-import common.UICommon;
+import common.UICommonTest;
 import org.testng.annotations.Test;
 
-public class DashboardsTests extends UICommon {
+public class DashboardsTests extends UICommonTest {
 	@Test(priority = 1)
 	public void validate(){
 		LoginPage loginPage = new LoginPage(driver).openPage();
